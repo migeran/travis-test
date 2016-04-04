bb5d8733f16c05c422d6a71c2a0f3924b8c8aa5f
Kristof Liliom <kristof.liliom@mattakis.com>
2016-04-04 12:52:33 +0200

    Updated deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,17 @@ chmod 600 scripts/id_rsa
 ssh-add -K scripts/id_rsa
 
 # Clone target repo
-git clone ssh://github.com/kovacsi/testrepo.git build-repo
+git clone gitosis@github.com:kovacsi/testrepo.git build-repo
 cd build-repo
+
+# Update git config
+echo "Host github" >> ~/.ssh/config
+echo -e "\tHostName github.com" >> ~/.ssh/config
+echo -e "\tUser git" >> ~/.ssh/config
+echo -e "\tIdentityFile ../scripts/id_rsa" >> ~/.ssh/config
+echo -e "\tIdentitiesOnly yes" >> ~/.ssh/config
+git remote add push-remote git@github:kovacsi/testrepo.git
+git fetch push-remote
 
 # Update files
 rm -rf TestSDK.framework
@@ -19,7 +28,7 @@ git add --all TestSDK.framework
 git config user.name "Travis CI"
 git config user.email "travis-ci@mattakis.com"
 git commit -m "$(cd ../ &&  git rev-parse HEAD)"
-git push origin master
+git push push-remote master
 
 # Clean up
 cd ..
