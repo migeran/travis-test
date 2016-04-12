66850c92448b235880afaa78d0a664278cadfaa2
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-04-12 17:20:16 +0200

    Updated commit message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -50,7 +50,7 @@ if [ -n "$TRAVIS_TAG" ]; then
 fi
 
 echo "Commiting..."
-git commit -m "Build: $TRAVIS_BUILD_NUMBER" -m "Repository: https://github.com/kovacsi/testrepo.git" -m "Branch: $TRAVIS_BRANCH" -m "Commit id: $TRAVIS_COMMIT"
+git commit -m "Build: $TRAVIS_BUILD_NUMBER" -m "Commit id: $TRAVIS_COMMIT ($TRAVIS_BRANCH)"
 
 echo "Pushing..."
 git push origin "$DIST_BRANCH_NAME"
