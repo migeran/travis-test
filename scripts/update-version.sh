0f0ddf1284ded6d0c59502eed6d371de4a0ad3cf
Kristof Liliom <kristof.liliom@mattakis.com>
2016-04-15 10:08:10 +0200

    Make set version and build number

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -3,5 +3,6 @@
 # Branch for tagged versions
 if [ -n "$TRAVIS_TAG" ]; then
 	echo "Updating framework version to $TRAVIS_TAG..."
-	agvtool -noscm new-version -all "$TRAVIS_TAG"
+	xcrun agvtool -noscm new-marketing-version "$TRAVIS_TAG"
 fi
+xcrun agvtool -noscm new-version -all "$TRAVIS_BUILD_NUMBER"
